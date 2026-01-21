d9afa9f09a2b34079672baf9e2e8db281952c491
edg <ngoufackedgard1@gmail.com>
2026-01-22 00:41:44 +0100

    improve style

--- server.js
+++ server.js
@@ -179,20 +179,3 @@ app.get("/api/stats", async (req, res) => {
     console.log(`Public URL: ${publicUrl}`);
   });
 })();
-
-
-// (async () => {
-//   await initDb();
-
-//   const publicUrl = process.env.RENDER_EXTERNAL_URL || "http://localhost";
-//   console.log(`Public URL: ${publicUrl}`);
-
-
-
-// //   const PORT = process.env.PORT || 10000; // 10000 en local si tu veux coller à Render
-// //   app.listen(PORT, "0.0.0.0", () => {
-// //     console.log(`Web + API listening on port ${https://satisfaction-survey-sfmtescnologies.onrender.com/}`);
-// //   });
-// })();
-
-
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./*.html",
+    "./**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
